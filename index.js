2e8681c3fb9688f19b155bf78e261aade6531ee0
Satara <tatenokay@gmail.com>
2023-06-02 17:24:22 -0500

    Eslint_Loggert

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 const express = require('express')
 const app = express()
 // const http = require('http')
+const logger = require('./loggerMiddleware')
 
-const notes = [
+app.use(express.json())
+
+app.use(logger)
+
+let notes = [
   {
     id: 1,
     content: 'varias cosas qeu sirbven de string en este objeto',
@@ -64,6 +69,25 @@ app.post('/api/notes/', (request, response) => {
   const note = request.body
 
   response.json(note)
+
+  const ids = notes.map(note => note.id)
+  const maxId = Math.max(...ids)
+
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    important: typeof note.important !== 'undefined' ? note.important : false,
+    date: new Date().toISOString()
+  }
+
+  notes = notes.concat(newNote)
+
+  response.status(201).json(newNote)
+})
+app.use((request, response) => {
+  response.status(404).json({
+    error: 'Not Found'
+  })
 })
 
 const PORT = 3002
